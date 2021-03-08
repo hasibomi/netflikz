5fa5014150e86d5bd93973be87f64cff2de88fb2
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2021-03-08 15:45:17 +0600

    main.py modified for better structure

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 
 
-os.environ["FLASK_APP"] = "netflikz"
-os.environ["FLASK_ENV"] = "development"
-os.system("env/bin/python -m flask run")
+def main():
+    os.environ["FLASK_APP"] = "netflikz"
+    os.environ["FLASK_ENV"] = "development"
+    os.system("env/bin/python -m flask run")
+
+
+if __name__ == "__main__":
+    main()
