3d6ce008a7f3bc567337f61c9fdbcd8ec0a5fca5
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2021-03-08 16:52:23 +0600

    Landing page added

--- netflikz/views.py
+++ netflikz/views.py
@@ -1,11 +1,9 @@
 from flask.views import View
+from flask import render_template
 
 
 class Home(View):
     methods = ["GET"]
 
     def dispatch_request(self):
-        return {
-            "status": "success",
-            "result": "ok"
-        }
+        return render_template("index.html")
