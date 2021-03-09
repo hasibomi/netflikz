6671a347f7683729c1da44f00a14eea35100b56d
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2021-03-09 17:45:32 +0600

    Added WTForms to handle form request

---
+++ netflikz/api/auth/forms.py
@@ -0,0 +1,20 @@
+from wtforms import fields
+from wtforms import validators
+from wtforms.fields import html5
+from wtforms.form import Form
+
+
+class FormSignUp(Form):
+    email = html5.EmailField(validators=[
+        validators.InputRequired(),
+        validators.Email(message="Email is required")
+    ])
+    password = fields.PasswordField(
+        validators=[
+            validators.InputRequired(message="Password is required"),
+            validators.EqualTo("password_confirmation", message="Passwords must match")
+        ]
+    )
+    password_confirmation = fields.PasswordField(
+        validators=validators.InputRequired(message="Password confirmation is required")
+    )
